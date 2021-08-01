332405273d13a88ae6b01781efeac2cf034e2cd1
andy salerno <andysalerno@gmail.com>
2021-08-01 16:16:41 -0700

    Fix fonts and others

--- card/mouseover.gd
+++ card/mouseover.gd
@@ -1,9 +1,17 @@
 extends StaticBody
 
+var state_rising = false
+var state_rising_target = false
+
 func _ready():
 	connect("input_event", self, "on_input_event")
 
 func on_input_event(camera, event, click_position, click_normal, shape_idx):
-	var mouse_click = event as InputEventMouseButton
-	if mouse_click and mouse_click.button_index == 1 and mouse_click.pressed:
-		print("clicked")
+	var mouse_event = event as InputEventMouse
+	if mouse_event:
+		if !state_rising:
+			state_rising_target = true
+		state_rising = true
+
+func _process(delta):
+	pass
